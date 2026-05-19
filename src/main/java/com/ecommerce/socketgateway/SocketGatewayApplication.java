d032a9036073e00a89c81717adc5afc6a9cf20bb
thang2k6adu <thang2k6adu@gmail.com>
2026-05-19 20:15:57 +0700

    config: enable auto scan configuration properties

--- src/main/java/com/ecommerce/socketgateway/SocketGatewayApplication.java
+++ src/main/java/com/ecommerce/socketgateway/SocketGatewayApplication.java
@@ -2,10 +2,10 @@ package com.ecommerce.socketgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class SocketGatewayApplication {
 
 	public static void main(String[] args) {
